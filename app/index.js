f7a4c10366907001b99ea448e2d7bcf620d5cbe5
Rebeca Calazans <rebecac.brito@gmail.com>
2025-06-29 14:05:28 -0700

    Add control buttons

--- app/index.js
+++ app/index.js
@@ -16,24 +16,30 @@ const State = {
     LONG_BREAK: 2
 }
 
-const WORK_TIME = .25; // 25 minutes
-const SHORT_BREAK = 1; // 5 minutes
-const LONG_BREAK = 1 //15 minutes;
+const WORK_TIME = .2; // 25 minutes
+const SHORT_BREAK = .2; // 5 minutes
+const LONG_BREAK = .2; //15 minutes;
 const STATE_TIMES = [WORK_TIME, SHORT_BREAK, LONG_BREAK];
 const STATE_COLORS = ["#B74949", "#38858A", "#397097"];
 
+let startTime = Date.now();
 let sessionCount = 0;
 let timeMissing = WORK_TIME;
 let currentState = State.WORK;
 let paused = true;
+let timeoutId;
 
 // Get const elements from document
+const background = document.getElementById("background");
 const countdown = document.getElementById("countdown");
 const countdown_text = countdown.getElementById("time-left");
 const countdown_progress = countdown.getElementById("progress-bar");
-const countdown_progress_middle = countdown.getElementById("progress-bar-middle");
-const startButton = document.getElementById("startButton");
+const pause_button = document.getElementById("pause-button");
+const play_button = document.getElementById("play-button");
+const skip_button = document.getElementById("skip-button");
 
+// Set initial state
+pause_timer();
 
 // Update the clock every minute
 clock.granularity = "seconds";
@@ -41,21 +47,20 @@ changeState(currentState);
 
 function setStateColor(state) {
     const color = STATE_COLORS[state];
-    console.log("Current state color:", color);
-    countdown_progress.style.fill = color;
-    countdown_progress_middle.style.fill = color;
+    background.style.fill = color;
 }
 
 function changeState(state) {
+    pause_timer();
     timeMissing = STATE_TIMES[state] * 60;
-    paused = true;
+    startTime = Date.now();
     setStateColor(state);
+    updateCountdown();
 }
 
 function startNextState() {
     switch (currentState) {
         case State.WORK:
-            vibration.start("alert");
             sessionCount++;
             if (sessionCount % 4 === 0) {
                 currentState = State.LONG_BREAK;
@@ -66,26 +71,83 @@ function startNextState() {
         case State.SHORT_BREAK:
         case State.LONG_BREAK:
             currentState = State.WORK;
-            vibration.start("confirmation");
             break;
     }
     changeState(currentState);
 }
 
+function startAlerting() {
+    vibration.start("alert");
+    setTimeout(() => {
+        vibration.stop();
+    }, 6000);
+}
+
+function endInterval() {
+    startAlerting();
+    startNextState();
+}
+
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-    paused = false;
     if (!paused) {
-        timeMissing -= 1;
-        if (timeMissing <= 0) {
-            startNextState();
-        }
+        updateCountdown();
+    }
+}
+
+function updateCountdown() {
+    let elapsedTime = (Date.now() - startTime) / 1000;
+    let time = Math.round(timeMissing - elapsedTime);
+
+    if (time <= 0) {
+        endInterval();
+        return;
     }
 
-    const minutes = Math.floor(timeMissing / 60);
-    const seconds = zeroPad(timeMissing % 60);
+    const minutes = Math.floor(time / 60);
+    const seconds = zeroPad(time % 60);
     countdown_text.text = `${minutes}:${seconds}`;
 
-    const sweep_angle = 360 * (1 - timeMissing / (STATE_TIMES[currentState] * 60));
+    const sweep_angle = 360 * (1 - time / (STATE_TIMES[currentState] * 60));
     countdown_progress.sweepAngle = sweep_angle;
+}
+
+// Buttons handling
+play_button.addEventListener("click", (evt) => {
+    continue_timer();
+});
+
+pause_button.addEventListener("click", (evt) => {
+    pause_timer();
+});
+
+skip_button.addEventListener("click", (evt) => {
+    console.log("Skip button pressed");
+    vibration.start("confirmation-max");
+    startNextState();
+});
+
+function setPaused(value) {
+    if (value) {
+        pause_timer();
+    } else {
+        continue_timer();
+    }
+}
+
+function pause_timer() {
+    clearTimeout(timeoutId);
+    paused = true;
+    play_button.style.display = "inherit";
+    pause_button.style.display = "none";
+    let elapsedTime = (Date.now() - startTime) / 1000;
+    timeMissing -= elapsedTime;
+}
+
+function continue_timer() {
+    paused = false;
+    pause_button.style.display = "inherit";
+    play_button.style.display = "none";
+    startTime = Date.now();
+    timeoutId = setTimeout(endInterval, timeMissing * 1000);
 }
